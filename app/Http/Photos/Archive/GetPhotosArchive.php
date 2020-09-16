2ebe6a3ea056b3e66f837ac1c7382d4746cfc8c5
mikka <ezsolt@gmail.com>
2020-09-16 22:44:55 +0200

    fixing loading of http action

---
+++ app/Http/Photos/Archive/GetPhotosArchive.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+namespace App\Http\Photos\Archive;
+
+class GetPhotosArchive
+{
+    public function __invoke(int $year = null, int $month = null)
+    {
+        // ...
+        echo '90897986786078';
+        echo 'year: ' . $year . ' / ' . 'month: ' . $month;
+    }
+}
+
+
+
+
